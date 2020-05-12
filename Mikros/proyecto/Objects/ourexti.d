f6561397861ac42301a80d1349d6e583845e12f8
iratiizagirre <irati.izagirre@alumni.mondragon.edu>
2020-05-12 15:39:37 +0200

    [MICROS] copia de proyecto

---
+++ Mikros/proyecto/Objects/ourexti.d
@@ -0,0 +1,10 @@
+.\objects\ourexti.o: ourExti.c
+.\objects\ourexti.o: ourExti.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f407xx.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\ourexti.o: C:\Keil_v5\ARM\PACK\Keil\STM32F4xx_DFP\2.14.0\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
---
+++ Mikros/proyecto/ourAdc.c
@@ -0,0 +1,32 @@
+#include "ourAdc.h"
+#include "ourRccGpio.h"
+
+
+void initAdc01(void)
+{
+	//ADC1,2,3 daude APB2-n
+	RCC->APB2ENR|= 0x01<<8;
+	//12 biteko erresoluzioa
+	ADC1->CR1 &= ~(0x03<<24); 
+	//Right alignment : datuak nola jasotzen diren (pisu txikitik handira)
+	ADC1->CR2 &= ~(0x01<<11);
+	//Aldaketa bakarra (ez kontinuoa)
+	ADC1->CR2 &= ~(0x01<<1);
+	//Zein kanaletan ematen da aldaketa hori : Kanal bakarretik 
+	ADC1->SQR1 &= ~(0x0F<<20);
+	//Zein konbertsiotan, zein kanal -> 1 konbertsioan (sqr3 offset gabe), 10 kanala (0x0A)
+	ADC1->SQR3 |= 0x0A;
+	//Zenbat flankoren ondoren egin konbertsioa? 15
+	//10 kanala SMPR1ean dago, offsetik gabe
+	ADC1->SMPR1 |= 0x01;
+	//Piztu ADC1
+	ADC1->CR2 |= 0x01;  
+}
+
+uint32_t sinchronousGetSample(void)
+{
+	ADC1->CR2 |= 0x01 <<30; //PIZTU KONBERTSIOAK!!! 
+	while(! (ADC1->SR & (0x01<<1)) ); //EOC flaga
+	return ADC1->DR;
+
+}
